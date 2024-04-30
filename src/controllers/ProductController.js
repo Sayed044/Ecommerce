d0052b78878277f1810199041eed5d1faad20458
Sayed Ahmd <60805556+pranto78@users.noreply.github.com>
2024-05-01 01:11:03 +0600

    first commit

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,75 @@
+const {
+    BrandListService, CategoryListService, SliderListService, ListByCategoryService, ListByBrandService, ListBySmilierService, ListByRemarkService, ListByKeywordService, DetailsService, ReviewListService
+} = require('../services/ProductServices')
+
+
+exports.ProductBrandList=async (req,res)=>{
+    let result=await BrandListService();
+    return res.status(200).json(result)
+}
+
+exports.ProductCategoryList=async (req,res)=>{
+    let result=await CategoryListService();
+    return res.status(200).json(result)
+}
+
+exports.ProductSliderList=async (req,res)=>{
+    let result=await SliderListService();
+    return res.status(200).json(result)
+}
+
+exports.ProductListByBrand=async (req,res)=>{
+    let result=await ListByBrandService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListByCategory=async (req,res)=>{
+    let result=await ListByCategoryService(req);
+    return res.status(200).json(result)
+}
+
+exports.ProductListByRemark=async (req,res)=>{
+    let result=await ListByRemarkService(req);
+    return res.status(200).json(result)
+}
+
+
+
+
+exports.ProductListBySmilier=async (req,res)=>{
+
+}
+
+exports.ProductListByKeyword=async (req,res)=>{
+
+}
+
+
+
+exports.ProductDetails=async (req,res)=>{
+
+}
+
+exports.ProductReviewList=async (req,res)=>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
